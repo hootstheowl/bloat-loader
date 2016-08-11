f20f5a2cc50496911f1c1247885eb178260ba226
Shawn Scofield <shawnscof@gmail.com>
2016-08-11 10:15:05 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Bloat loader for [webpack](http://webpack.github.io/)
+
+bloat-loader is a webpack loader that will replace all require() and import declarations with the requested file's source code. Effectively, bloating your code from this:
+``` javascript
+import FooBar from 'app/directory/FooBar';
+
+const instance = new FooBar();
+instance.init();
+```
+...into something like this:
+``` javascript
+class FooBar {
+  constructor() {
+    this.foo = 'bar';
+  }
+  init() {
+    return this.foo;
+  }
+}
+
+const instance = new FooBar();
+instance.init();
+```
+
+## Install
+
+`npm install bloat-loader --save-dev`
+
+## Usage
+
+### Intended Usage
+This loader should (probably) never be applied to an entire project and is intended for use cases where compiling a class or method's dependencies into one source is particularly useful (e.g. when creating a [Bookmarklet](https://en.wikipedia.org/wiki/Bookmarklet)).
+
+### Apply via webpack config
+
+``` javascript
+module.exports = {
+  ...
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['bloat-loader'],
+        include: path.join(__dirname, 'app/myBloatedFiles')
+      }
+    ]
+  }
+};
+```
+When using [Babel](https://babeljs.io/) or another transpiler, bloat-loader must be applied beforehand so that it's result can be transpiled:
+``` javascript
+...
+        loaders: ['babel', 'bloat-loader'],
+...
+``` 
---
+++ index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var fs = require('fs');
+var loaderUtils = require('loader-utils');
+
+function BloatLoader(filename, source) {
+  this.filename = filename;
+  this.source = source;
+  this.matchRules = [
+    // import something from 'my/directory'
+    /^import\s[\s{]*(\w[,\s\w]*)[\s}]*\sfrom\s+['"](.*)['"].*/gm,
+    // import 'my/directory'
+    /^import\s['"](.*)['"].*/gm,
+    // var something = require('my/directory')
+    /^(?:var|let|const)\s+(\w+)\s*=\s*require\(['"](.*)['"]\).*/gm,
+    // require('my/directory')
+    /^require\(['"](.*)['"]\).*/gm,
+  ];
+};
+
+BloatLoader.prototype.init = function() {
+  var result = this.source;
+  for (var i = 0; i < this.matchRules.length; i++) {
+    var regexp = this.matchRules[i];
+    var matches = this.getMatches(this.source, regexp);
+    if (matches.length > 0) {
+      result = this.bloatMatches(result, matches);
+    }
+  }
+  return result;
+};
+
+BloatLoader.prototype.getMatches = function(source, regexp) {
+  var matches = [];
+  var match;
+  while ((match = regexp.exec(source)) !== null) {
+    matches.push(match);
+  }
+  return matches;
+};
+
+BloatLoader.prototype.bloatMatches = function(source, matches) {
+  var nextSource = source;
+  for (var i = 0; i < matches.length; i++) {
+    var match = matches[i];
+    var matchFile = this.getRelativeFilename(
+      this.filename, match[2]
+    );
+    var file = fs.readFileSync(matchFile, 'utf8');
+    if (!file) {
+      continue;
+    }
+    nextSource = nextSource.replace(match[0], file);
+  }
+  return nextSource;
+};
+
+BloatLoader.prototype.getRelativeFilename = function(
+  sourceFilename, relativeFilename
+) {
+  var lowerDirectories = relativeFilename.match(/^(\.\.\/)/);
+  var newFilename = sourceFilename;
+  for (var i = 0; i < lowerDirectories.length; i++) {
+    if (lowerDirectories[i] === '') {
+      continue;
+    }
+    newFilename = newFilename.replace(/(\/[^/]+)$/, '');
+  }
+  return newFilename+'/'+relativeFilename.replace(/^(\.\.\/)+/, '')+'.js';
+};
+
+module.exports = function(source, map) {
+  this.cacheable && this.cacheable();
+  var result = '';
+  var callback = this.async();
+  var webpackRemainingChain = loaderUtils.getRemainingRequest(this).split('!');
+  var filename = webpackRemainingChain[webpackRemainingChain.length - 1];
+  var loader = new BloatLoader(filename, source);
+  try {
+    result = loader.init();
+  } catch (e) {
+    throw new Error(e);
+  }
+  this.callback(null, loader.bloat();, map);
+};
